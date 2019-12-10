705dc3b747013e1d45b12ad0c11b652fd97a0c32
juan <jupegarnica@gmail.com>
2019-12-10 11:07:41 +0100

    passwrod validation done as service

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,28 @@
 const { User } = require('../models');
+const { isValidPassword } = require('../services/validations');
 
 async function registerController(req, res, next) {
   try {
+    isValidPassword(req.body.password);
     const user = await User.create(req.body);
     res.status(200).json({
       message: 'register done',
       user: user,
     });
   } catch (error) {
+      console.log(error);
+
+    if (error.message === 'invalidPasswordError') {
+      return res.status(400).json({
+        message: 'invalid password',
+        error: error,
+      });
+    }
     if (error.name === 'SequelizeUniqueConstraintError') {
-     return   res.status(400).json({
-          message: 'register invalid',
-          error: error.errors[0].message,
-        });
+      return res.status(400).json({
+        message: 'register invalid',
+        error: error.errors[0].message,
+      });
     }
     if (error.name === 'SequelizeValidationError') {
       return res.status(400).json({
@@ -20,11 +30,10 @@ async function registerController(req, res, next) {
         error: error.errors[0].message,
       });
     }
-       res.status(400).json({
-         message: 'register invalid',
-         error: error
-       });
-
+    res.status(400).json({
+      message: 'register invalid',
+      error: error,
+    });
   }
 }
 
